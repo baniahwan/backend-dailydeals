6a8bd6609a8238a3fff1c5e8bee39283649c9ce1
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-09-28 21:43:02 +0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "dailydeals_db"
+    host: "bkgntnnc680exhv5pimh-mysql.services.clever-cloud.com",
+    user: "uifahzima0rwanpx",
+    password: "igiT297Qele9mZrl089L",
+    database: "bkgntnnc680exhv5pimh",
+    port: 3306
 })
 
 module.exports = db
